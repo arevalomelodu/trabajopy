2a741490deef4ab6477460c61e862c280c3fc5c0
arevalomelodu <134864043+arevalomelodu@users.noreply.github.com>
2023-06-02 00:43:14 +0000

    comando re

---
+++ regulares3.py
@@ -0,0 +1,7 @@
+import re
+
+sub_cadena= re.search("\d\d\d","hola789mundo57!")
+# con el comando \d\d\d estoy buscando tres números consec
+#sub_cadena = re.search(r"\d\d", "hola789mundo57!")
+#sub_cadena re.search(r"\d\d", "hola7mundo57!")
+print (sub_cadena.group())
